769aa58cb1e4eebee2f98fcea6ca2d2bd2e49575
EC2 Default User <ec2-user@ip-172-31-46-188.ap-northeast-1.compute.internal>
2020-04-20 08:20:10 +0000

    fix profile layouts３

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
   <head>
-    <title>{{ config('app.name', 'Laravelgram') }}</title>
+    <title>オリンピックサッカー応援サイト</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1">
